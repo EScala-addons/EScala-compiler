EScala-addons/EScala-compiler

commit 30a391330247d5708513a1d1db2bf63186f57347

fixed #226 and #227
git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@13296 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/symtab/Types.scala
@@ -1320,10 +1320,14 @@ trait Types {
       sym.isModuleClass || sym == AllClass || isValueClass(sym) || super.isNotNull
 
     // @M: propagate actual type params (args) to `tp', by replacing formal type parameters with actual ones
-    def transform(tp: Type): Type =
-      tp.asSeenFrom(pre, sym.owner).instantiateTypeParams(sym.typeParams, argsMaybeDummy)
+    def transform(tp: Type): Type = {
+      val args = argsMaybeDummy
+      if (args.length == sym.typeParams.length)
+        tp.asSeenFrom(pre, sym.owner).instantiateTypeParams(sym.typeParams, argsMaybeDummy)
+      else { assert(args exists (_.isError)); tp }
       // @M TODO maybe we shouldn't instantiate type params if isHigherKinded -- probably needed for partial type application though
-      
+    }
+
     //@M! use appliedType on the polytype that represents the bounds (or if aliastype, the rhs)
     def transformInfo(tp: Type): Type =
       appliedType(tp.asSeenFrom(pre, sym.owner), argsMaybeDummy)
@@ -3552,7 +3556,7 @@ A type's typeSymbol should never be inspected directly.
                 else {
                   def lubBounds(bnds: List[TypeBounds]): TypeBounds =
                     mkTypeBounds(glb(bnds map (_.lo), depth-1), lub(bnds map (_.hi), depth-1))
-                  recycle(proto.owner.newAbstractType(proto.pos, proto.name))
+                  recycle(lubRefined.typeSymbol.newAbstractType(proto.pos, proto.name))
                     .setInfo(lubBounds(symtypes map (_.bounds)))
                 }
               }

src/compiler/scala/tools/nsc/symtab/classfile/Pickler.scala
@@ -162,7 +162,8 @@ abstract class Pickler extends SubComponent {
           putType(lo); putType(hi)
         case RefinedType(parents, decls) =>
           val rclazz = tp.typeSymbol
-          assert(decls.elements forall (_.owner == rclazz))
+          for (m <- decls.elements)
+            if (m.owner != rclazz) assert(false, "bad refinement member "+m+" of "+tp+", owner = "+m.owner)
           putSymbol(rclazz); putTypes(parents); putSymbols(decls.toList)
         case ClassInfoType(parents, decls, clazz) =>
           putSymbol(clazz); putTypes(parents); putSymbols(decls.toList)

test/files/pos/t0227.scala
@@ -1,0 +1,31 @@
+final class Settings {
+    def f[T](a_args: T*): List[T] = Nil
+}
+
+abstract class Factory {
+    type libraryType <: Base
+
+    final def apply(settings: Settings): libraryType = error("bla")
+}
+
+abstract class Base {
+    val settings: Settings
+
+    protected val demands: List[Factory] = Nil
+}
+
+class SA(val settings: Settings) extends Base {
+    override val demands =  List(
+            SD
+        ) :::  settings.f(
+            SC
+        )    
+}
+
+object SC extends Factory {
+    type libraryType = Base
+}
+
+object SD extends Factory {
+    type libraryType = SA
+}

commit 02bd7d16bc4696fa5c8ecbb966ab6e5d1633087b

fixed #231. fixed regression errors in r13296. added new tests.
git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@13299 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/symtab/Types.scala
@@ -1324,7 +1324,7 @@ trait Types {
       val args = argsMaybeDummy
       if (args.length == sym.typeParams.length)
         tp.asSeenFrom(pre, sym.owner).instantiateTypeParams(sym.typeParams, argsMaybeDummy)
-      else { assert(args exists (_.isError)); tp }
+      else { assert(sym.typeParams.isEmpty || (args exists (_.isError)), tp); tp }
       // @M TODO maybe we shouldn't instantiate type params if isHigherKinded -- probably needed for partial type application though
     }
 
@@ -3760,6 +3760,8 @@ A type's typeSymbol should never be inspected directly.
       } catch {
         case ex: MalformedType => None
       }
+    case _ =>
+      assert(false, tps); None
   }
 
   /** Make symbol `sym' a member of scope `tp.decls'

src/compiler/scala/tools/nsc/transform/UnCurry.scala
@@ -49,43 +49,49 @@ abstract class UnCurry extends InfoTransform with TypingTransformers {
   private def expandAlias(tp: Type): Type = if (!tp.isHigherKinded) tp.normalize else tp
   
   private val uncurry: TypeMap = new TypeMap {
-    def apply(tp0: Type): Type = {val tp=expandAlias(tp0); tp match {
-      case MethodType(formals, MethodType(formals1, restpe)) =>
-        apply(MethodType(formals ::: formals1, restpe))
-      case MethodType(formals, ExistentialType(tparams, restpe @ MethodType(_, _))) =>
-        assert(false, "unexpected curried method types with intervening exitential") 
-        tp0
-      case mt: ImplicitMethodType =>
-        apply(MethodType(mt.paramTypes, mt.resultType))
-      case PolyType(List(), restpe) =>
-        apply(MethodType(List(), restpe))
-      case PolyType(tparams, restpe) =>
-        PolyType(tparams, apply(MethodType(List(), restpe)))
-      /*
-      case TypeRef(pre, sym, List(arg1, arg2)) if (arg1.typeSymbol == ByNameParamClass) =>
-        assert(sym == FunctionClass(1))
-        apply(typeRef(pre, definitions.ByNameFunctionClass, List(expandAlias(arg1.typeArgs(0)), arg2)))
-      */
-      case TypeRef(pre, sym, List(arg)) if (sym == ByNameParamClass) =>
-        apply(functionType(List(), arg))
-      case TypeRef(pre, sym, args) if (sym == RepeatedParamClass) =>
-        apply(rawTypeRef(pre, SeqClass, args))
-      case _ =>
-        expandAlias(mapOver(tp))
-    }}
+    def apply(tp0: Type): Type = {
+      val tp = expandAlias(tp0)
+      tp match {
+        case MethodType(formals, MethodType(formals1, restpe)) =>
+          apply(MethodType(formals ::: formals1, restpe))
+        case MethodType(formals, ExistentialType(tparams, restpe @ MethodType(_, _))) =>
+          assert(false, "unexpected curried method types with intervening exitential") 
+          tp0
+        case mt: ImplicitMethodType =>
+          apply(MethodType(mt.paramTypes, mt.resultType))
+        case PolyType(List(), restpe) =>
+          apply(MethodType(List(), restpe))
+        case PolyType(tparams, restpe) =>
+          PolyType(tparams, apply(MethodType(List(), restpe)))
+        /*
+         case TypeRef(pre, sym, List(arg1, arg2)) if (arg1.typeSymbol == ByNameParamClass) =>
+         assert(sym == FunctionClass(1))
+         apply(typeRef(pre, definitions.ByNameFunctionClass, List(expandAlias(arg1.typeArgs(0)), arg2)))
+         */
+        case TypeRef(pre, sym, List(arg)) if (sym == ByNameParamClass) =>
+          apply(functionType(List(), arg))
+        case TypeRef(pre, sym, args) if (sym == RepeatedParamClass) =>
+          apply(rawTypeRef(pre, SeqClass, args))
+        case _ =>
+          expandAlias(mapOver(tp))
+      }
+    }
   }
 
   private val uncurryType = new TypeMap {
-    def apply(tp0: Type): Type = {val tp=expandAlias(tp0); tp match {
-      case ClassInfoType(parents, decls, clazz) =>
-        val parents1 = List.mapConserve(parents)(uncurry)
-        if (parents1 eq parents) tp
-        else ClassInfoType(parents1, decls, clazz) // @MAT normalize in decls??
-      case PolyType(_, _) =>
-        mapOver(tp)
-      case _ =>
-        tp
-    }}
+    def apply(tp0: Type): Type = {
+      val tp = expandAlias(tp0)
+      tp match {
+        case ClassInfoType(parents, decls, clazz) =>
+          val parents1 = List.mapConserve(parents)(uncurry)
+          if (parents1 eq parents) tp
+          else ClassInfoType(parents1, decls, clazz) // @MAT normalize in decls??
+        case PolyType(_, _) =>
+          mapOver(tp)
+        case _ =>
+          tp
+      }
+    }
   }
   
   /** - return symbol's transformed type, 

src/compiler/scala/tools/nsc/typechecker/SuperAccessors.scala
@@ -151,7 +151,13 @@ abstract class SuperAccessors extends transform.Transform with transform.TypingT
               clazz.newMethod(tree.pos, supername)
                 .setFlag(SUPERACCESSOR | PRIVATE)
                 .setAlias(sym)
-            superAcc.setInfo(clazz.thisType.memberType(sym).cloneInfo(superAcc))
+            var superAccTpe = clazz.thisType.memberType(sym) 
+            if (sym.isModule && !sym.isMethod) {
+              // the super accessor always needs to be a method. See #231
+              superAccTpe = PolyType(List(), superAccTpe)
+            }
+            superAcc.setInfo(superAccTpe.cloneInfo(superAcc))
+            //println("creating super acc "+superAcc+":"+superAcc.tpe)//DEBUG
             clazz.info.decls enter superAcc;
             accDefBuf(clazz) += typed(DefDef(superAcc, vparamss => EmptyTree))
           }

test/files/neg/t0226.scala
@@ -1,0 +1,9 @@
+class Test {
+  def foo[A](x: A)(implicit rep: Foo[A]): Foo[A] = rep
+  abstract class Foo[A]
+  implicit def list2Foo[List[A1], A2]
+      (implicit _1: Foo[List[A1]], _2: Foo[A2]): Foo[Tuple2[List[A1], A2]] =
+    null //dummy
+
+  foo(((List('b'), 3), (Nil, 4)))
+}

test/files/pos/t0231.scala
@@ -1,0 +1,17 @@
+class Foo {
+  object aaa {
+    println("a")
+  }
+}
+
+class Bar extends Foo {
+  object b {
+    //println("b: " + a) //OK
+    println("b: " + Bar.super.aaa)
+  }
+}
+
+object bug extends Application {
+  new Bar
+  ()
+}