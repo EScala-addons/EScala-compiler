3a356f67285273933658d93af05b150dcdc65d37
Lucas Satabin <satabin@st.informatik.tu-darmstadt.de>
2010-07-22 13:52:51 +0200

    `between' operator and associated EventNode class added. Semantics:   evt e = e1 between(e2, e3) `e' is triggered whenever `e1' is triggered between an occurrence of `e2' and `e3'

---
+++ test/files/events-run/between-drag-n-drop.scala
@@ -0,0 +1,30 @@
+object Test {
+
+  // built-in events from the mouse
+  imperative evt mouseDown[Int, Int]
+  imperative evt mouseUp[Int, Int]
+  imperative evt mouseMove[Int, Int]
+
+  // combined events
+  evt drag[Int, Int] = mouseMove between(mouseDown, mouseUp)
+  evt drop[Int, Int] = (drag.dropParam then mouseUp).map((_: Unit, p: (Int, Int)) => p)
+
+  def main(args: Array[String]) {
+    mouseDown += {(x: Int, y: Int) => println("mouse down at (" + x + ", " + y + ")")}
+    mouseUp += {(x: Int, y: Int) => println("mouse up at (" + x + ", " + y + ")")}
+    mouseMove += {(x: Int, y: Int) => println("mouse move to (" + x + ", " + y + ")")}
+    drag += {(x: Int, y: Int) => println("mouse drag to (" + x + ", " + y + ")")}
+    drop += {(x: Int, y: Int) => println("mouse drop at (" + x + ", " + y + ")")}
+
+    mouseDown(1, 1)
+    mouseUp(1, 1)
+    mouseMove(2, 2)
+    mouseDown(2, 2)
+    mouseMove(2, 3)
+    mouseMove(2, 4)
+    mouseUp(2, 4)
+    mouseMove(2, 5)
+  }
+}
+
+// vim: set ts=2 sw=2 et:
