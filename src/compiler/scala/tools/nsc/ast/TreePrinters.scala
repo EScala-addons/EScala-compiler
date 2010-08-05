77e706e0059183d708b7f1d574365b4633344bb2
Lucas Satabin <satabin@st.informatik.tu-darmstadt.de>
2010-08-05 12:57:42 +0200

    case added for printing execution event

--- src/compiler/scala/tools/nsc/ast/TreePrinters.scala
+++ src/compiler/scala/tools/nsc/ast/TreePrinters.scala
@@ -205,6 +205,7 @@ trait TreePrinters { trees: SymbolTable =>
           kind match {
             case BeforeExec() => print("beforeExec(")
             case AfterExec() => print("afterExec(")
+            case Execution() => print("execution(")
           }
           print(meth)
           print(")")
