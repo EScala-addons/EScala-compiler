3a4a317a702dc65a531b26519da089e6f3547eb4
Lucas Satabin <lucassat@n7mm.org>
2010-09-23 13:29:31 +0200

    no event trace

--- src/library/scala/events/package.scala
+++ src/library/scala/events/package.scala
@@ -6,10 +6,6 @@ package object events {
 
   def within[T,U](ie: IntervalEvent[T,U]) = new WithinEvent(ie)
 
-  def causedBy[T](e: Event[T]) = new CausedByFilter(e)
-
-  def ?[T](e: =>Event[T]) = new EventNodeCond(e)
-
 }
 
 // vim: set ts=2 sw=2 et:
