ca9265f6bfe2505f2b64dee45cfcf822fe91f608
Michael Tretter <michael@fam-tretter.de>
2010-12-08 11:08:27 +0100

    add EVENT-flag and ErrorType to Event in Namer
    todo: substitute ErrorType by EventType

--- src/compiler/scala/tools/nsc/typechecker/Namers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -450,6 +450,8 @@ trait Namers { self: Analyzer =>
           //@ESCALA
           case EventDef(mods, name, tparams, _) =>
             tree.symbol = owner.newEvent(tree.pos, name).setFlag(mods.flags)
+            // TODO what value has to be set as info?
+            setInfo(sym)(namerOf(sym).typeCompleter(tree))
             //finishWith(tparams)
           //@ESCALA END
           case _ =>
@@ -930,6 +932,11 @@ trait Namers { self: Analyzer =>
       })
     }
 
+    private def eventSig(mods: Modifiers, vparams: List[ValDef]) {
+      // todo return correct Type
+      ErrorType
+    }
+
     /**
      * For every default argument, insert a method computing that default
      *
@@ -1154,6 +1161,12 @@ trait Namers { self: Analyzer =>
 
             case DefDef(mods, _, tparams, vparamss, tpt, rhs) =>
               newNamer(context.makeNewScope(tree, sym)).methodSig(mods, tparams, vparamss, tpt, rhs)
+             
+            // @ESCALA
+            case EventDef(mods, _, vparams, _) =>
+              // todo return EventType
+              newNamer(context.makeNewScope(tree, sym)).eventSig(mods, vparams)
+            // @ESCALA END
 
             case vdef @ ValDef(mods, name, tpt, rhs) =>
               val typer1 = typer.constrTyperIf(sym.hasFlag(PARAM | PRESUPER) && !mods.isJavaDefined && sym.owner.isConstructor)
