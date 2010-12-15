20621064cfe264d2b3251babd4e1dc8b5d8b8ebd
Michael Tretter <michael@fam-tretter.de>
2010-12-15 15:43:29 +0100

    Make test more expressive, rename file
    I modified the test, that it corresponds to the syntax that should
    already work. It does not work.
    I also added some comments to the test and renamed the file to a more
    meaningfull name.

--- test/files/events-exp-lang/rectangle.scala
+++ test/files/events-exp-lang/rectangle.scala
@@ -1,7 +1,8 @@
 import scala.events._
 
 class Point(var x: Int, var y: Int){    
-//  evt moved() = afterExec(moveBy)
+  // Moved event returns the new position
+  evt moved[(Int,Int)] = afterExec(moveBy).map((_:Any) => (x,y))
 
   def moveBy(dx: Int, dy: Int) = {
     x += dx
@@ -10,8 +11,14 @@ class Point(var x: Int, var y: Int){
 }
 
 class Rectangle(val upperleft: Point, val lowerright: Point){
-  evt resized(dx: Int, dy: Int) = lowerright.moved()
-//  evt moved() = upperleft.moved()
+  // Resized reacts on movement of the lowerright corner.
+  // This shows the new Syntax.
+  // compiler error: type events.this.Event[...] does not take parameter
+  evt resized(dx: Int, dy: Int) = lowerright.moved/*(dx,dy)*/
+
+  // moved reacts on movement of the upperleft corner
+  // this is the old syntax for comparison in the ast
+  evt moved[(Int,Int)] = upperleft.moved
 
   def resizeBy(dx: Int, dy: Int) = {
     lowerright.moveBy(dx, dy)
